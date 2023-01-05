17178db9ec0ea3c710364761681645d2baac524a
MorfoOficial <121363105+MorfoOficial@users.noreply.github.com>
2023-01-04 21:09:22 -0600

    Add files via upload

---
+++ bingo.php
@@ -0,0 +1,175 @@
+<script src="presentacion/js/propio/generador.js"></script>
+<center>
+<div id="tabgenerador" class="card-rows justify-content-center" style="margin-bottom: 15px;">
+  	<div class="card" style="min-width: 765px;max-width: 765px;">   
+  	 
+			
+	
+			
+			
+			
+		<ul class="card-header nav nav-tabs d-flex bg-master text-master justify-content-between rounded-top" id="tabgenerador" role="tablist" style="margin: 0px;padding: 0px ; border: 0px;">
+
+		  	<li class="nav-item">
+                    
+		    	<a class="nav-link" id="inicio-tab" data-toggle="tab" href="#inicio" role="tab" aria-controls="inicio" aria-selected="true" style="border: 0px;padding: 0px">&nbsp;<span class="glyphicon glyphicon-knight " aria-hidden="true" style="margin: 12px;"></span>&nbsp;</a>
+
+		  	</li>
+
+		  	<li class="nav-item ">
+			    <span class="btn btn-block"><strong class="text-master">Generador de Cartones de Bingo</strong></span>
+			</li>
+
+
+
+			  	<li class="nav-item">
+			    	<a class="nav-link active" id="configuracion-tab" data-toggle="tab" href="#configuracion" role="tab" aria-controls="configuracion" aria-selected="false" style="border: 0px;padding: 0px">&nbsp;<span class="glyphicon glyphicon-cog" aria-hidden="true" style="margin: 12px;"></span>&nbsp;</a>
+			  	</li>
+		</ul>
+		
+		
+
+  	  
+  	 	<div class="card-body text-center">	
+
+			<div class="tab-content justify-content-center" id="tabgeneradorcontenido">
+
+			  	<div class="tab-pane fade" id="inicio" role="tabpanel" aria-labelledby="inicio-tab">
+
+					<div id="contenidolistadecartones">
+
+					</div>
+
+				</div>				
+
+			 	<div class="tab-pane fade show active" id="configuracion" role="tabpanel" aria-labelledby="configuracion-tab">	
+			 		<label class="text-primary"><strong>Cantidad de Cartones</strong></label>
+				 	<br>
+
+				 	 <div class="form-group">
+					    <select id="cantidadcartones"  class="custom-select" required>
+					      <option value="10">10</option>
+					      <option value="20">20</option>
+					      <option value="30" selected="">30</option>
+					      <option value="40">40</option>
+					      <option value="50">50</option>
+					      <option value="60">60</option>
+					    </select>
+					</div>
+
+
+					
+			 	
+
+				 	 <div class="form-group" hidden>
+					    <select id="tipocarta" class="custom-select" required>
+					   	    <option value="90">Bingo 90</option>
+					    </select>
+					  </div>
+
+					<label class="text-primary"><strong>Fondo de Carton</strong></label>
+				 	<br>
+
+			 	 	<div class="form-group">
+					   	<input id="fondodecarton" class="custom-select text-primary" value="https://i.imgur.com/j25Q3mN.jpg" placeholder="URL de Fondo de Carton">
+					    </select>
+				  	</div>
+				  	
+
+					<br>
+
+
+			 	 	<center>
+
+			 	 		<button onclick="GuardarConfiguracionBingo();"  class="btn text-master bg-master">
+
+							Generar Cartones
+
+						</button>
+
+			 	 	</center>
+
+					
+
+			 	 	
+
+			 	</div>  	      	
+
+  	    	</div>
+
+  	 	</div>
+
+  	</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  	</div>
+  	</center>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- ======================== MODAL ======================== -->  
+
+<!-- VentanaEmergente -->
+
+<div class="modal fade" id="VentanaModalcarton" role="dialog">
+  	<div class="modal-dialog modal-lg">   
+
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header" hidden>
+          <h4 class="modal-title">
+			<strong>CARTON N° <span id="divmodaltitulo"></span></strong>
+		  </h4>
+        </div>
+
+        <div class="modal-body">
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+			<div class="d-flex justify-content-center">				
+				<div class="card bg-dark text-white" style="min-width: 340px;min-height: 250px;max-width: 340px;">
+				  <img class="card-img" id="modalfondocarton" src="presentacion/multimedia/imagen/transparentetablabingo.png" alt="Fondo De Carton">
+				  <div class="card-img-overlay" style="padding-left: 10px;padding-right: 10px;padding-top: 10px;padding-bottom: 10px;">
+				    <p class="card-text" id="divmodalcontenidocarton" style="margin-bottom: 8px;margin-top: 100px;">
+					</p>
+					<table align="center" width="300px">
+						<tr>
+							<td width="270px"></td>
+							<td width="30px" align="center"><h4 style="margin-bottom: 0px;"><span id="spannumerocarton"></span></h4></td>
+						</tr>
+					</table>
+
+				    <div class="d-flex justify-content-around">
+						<span></span><span></span>
+						<span></span><span id="divmodaltitulo" class="text-light"></span>
+					</div>
+				  </div>
+				</div>
+			</div>		
+	    </div>   
+    </div>
+</div>  
